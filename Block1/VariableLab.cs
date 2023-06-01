f5dfc7cf785bc75def8bdfd5cb1cecffbf6f2e96
Dylan Acup <dacup@ciscocems.com>
2023-05-31 19:19:23 -0600

    stuff

---
+++ Block1/VariableLab.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Block1
+{
+    class VariableLab
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+			
+
+
+
+
+        } //end method
+    }//end class
+}//end namespace
