8ecfb63306060568dfcb41deb950712c5f1f4e30
Dylan Acup <dacup@ciscocems.com>
2023-05-31 19:35:48 -0600

    Add solution for Variable Lab

---
+++ Block1/VariableLab_Solution.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block1
+{
+	internal class VariablesLab
+	{
+		static void Main(string[] args)
+		{
+			string name = "Jane Doe";
+			Console.WriteLine(name);
+
+			string holiday = "Halloween";
+			Console.WriteLine(holiday);
+
+			char at = '@';
+			Console.WriteLine(at);
+
+			char and = '&';
+			Console.WriteLine(and);
+
+			bool isCold = true;
+			Console.WriteLine(isCold);
+
+			bool didWinFirstPlace = true;
+			Console.WriteLine(didWinFirstPlace);
+
+			decimal weeklyPay = 900m;
+			Console.WriteLine(weeklyPay);
+
+			decimal yearlyProfit = 123456.76m;
+			Console.WriteLine(yearlyProfit);
+
+			float gpa = 2.5f;
+			Console.WriteLine(gpa);
+
+			float milesPerGallon = 7.8f;
+			Console.WriteLine(milesPerGallon);
+
+			double largeNumber = 1234954828756.485975498;
+			Console.WriteLine(largeNumber);
+
+			double calculatedVelocity = 485.7655432;
+			Console.WriteLine(calculatedVelocity);
+
+
+
+			byte byteMin = 0;
+			Console.WriteLine(byteMin);
+
+			byte byteMax = 255;
+			Console.WriteLine(byteMax);
+
+			sbyte sbyteMin = -128;
+			Console.WriteLine(sbyteMin);
+
+			sbyte sbyteMax = -127;
+			Console.WriteLine(sbyteMax);
+
+
+
+			short shortMin = -32000;
+			Console.WriteLine(shortMin);
+
+			short shortMax = 32000;
+			Console.WriteLine(shortMax);
+
+			ushort ushortMin = 0;
+			Console.WriteLine(ushortMin);
+
+			ushort ushortMax = 65000;
+			Console.WriteLine(ushortMax);
+
+
+
+			int intMin = -2000000000;
+			Console.WriteLine(intMin);
+
+			int intMax = 2000000000;
+			Console.WriteLine(intMax);
+
+			uint uintMin = 0;
+			Console.WriteLine(uintMin);
+
+			uint uintMax = 4000000000;
+			Console.WriteLine(uintMax);
+
+
+
+			long longMin = -9000000000000000000;
+			Console.WriteLine(longMin);
+
+			long longMax = 9000000000000000000;
+			Console.WriteLine(longMax);
+
+			ulong ulongMin = 0;
+			Console.WriteLine(ulongMin);
+
+			ulong ulongMax = 18000000000000000000;
+			Console.WriteLine(ulongMax);
+		}
+	}
+}
