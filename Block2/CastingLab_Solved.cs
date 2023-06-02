f3c7be5973dc2fb8619cea2068bef5554528f3af
Dylan Acup <dacup@ciscocems.com>
2023-06-01 19:09:11 -0600

    Add Casting Lab Solved and Operators Lab Solved

---
+++ Block2/CastingLab_Solved.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block2
+{
+	internal class CastingLabSolved
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Casting Lab");
+
+			byte puddle = 100;
+			short pond = 29456;
+			int lake = 100000; //one hundred thousand
+			long ocean = 5;
+
+			//1. cast the value of lake into ocean and display it
+			//2. cast the value of pond into puddle and display it
+			//3. cast the value of pond into ocean and display it
+			//4. cast the value of ocean into lake and display it
+			//5. cast the value of puddle into ocean and display it
+
+			//for each casting, include a comment categorizing it as:
+			//implicit casting
+			//explicit casting
+			//explicit casting with truncation
+
+			//1. lake into ocean
+			ocean = lake;//implicit
+			Console.WriteLine("Ocean is now " + ocean);
+
+			//2. pond into puddle 
+			puddle = (byte)pond;//explicit casting with truncation
+			Console.WriteLine("Puddle is now " + puddle);
+
+			//3. pond into ocean
+			ocean = pond;//implicit
+			Console.WriteLine("Ocean is now " + ocean);
+
+			//4. ocean into lake
+			lake = (int)ocean;//explicit casting
+			Console.WriteLine("Lake is now " + lake);
+
+			//5. puddle into ocean
+			ocean = puddle; //implicit
+			Console.WriteLine("Ocean is now " + ocean);
+		}
+	}
+}
---
+++ Block2/OperatorsLab_Solved.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block2
+{
+	internal class OperatorsLabSolved
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Operators Lab");
+
+			//1
+			byte square = 75;
+
+			Console.WriteLine("The permieter of the square is " + (4 * square));
+
+			//2
+			byte cuboidHeight = 45;
+			byte cuboidDepth = 20;
+			byte cuboidLength = 65;
+
+			Console.WriteLine("The volume of a cuboid that is 45in high, 20in deep, and 65in long is " + (cuboidDepth * cuboidHeight * cuboidLength));
+
+			//3
+			int vanCapacity = 9;
+			int passengers = 146;
+			Console.WriteLine("There were " + (passengers / vanCapacity) + " transport vans. There are still " + (passengers % vanCapacity) + " passengers that need to be transported.");
+
+			//4
+			byte girlScoutCookies = 5;
+			Console.WriteLine("The girl scout started out with " + girlScoutCookies);
+			girlScoutCookies -= 2;
+			Console.WriteLine("The girl scout is taking " + girlScoutCookies + " to the next house.");
+
+			//5
+			int aliens = 5;
+
+			Console.WriteLine(aliens-- + " aliens came to earth but one went home. There were then 4 aliens. Two more aliens came to earth which means there were a total of "
+				+ (aliens += 2) +
+				". Another alien came to earth making the total " + aliens++ + ".  Most of the aliens formed pairs and traveled to other planets but there was "
+				+ (aliens %= 2) + " alient left alone on earth.");
+
+		}//end Main()
+	}//end Class
+}//end Namespace
