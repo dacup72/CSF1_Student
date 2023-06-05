ee542737ef7f229bed3a384b0f3f17bc6a02a0b8
Dylan Acup <dacup@ciscocems.com>
2023-06-05 17:21:10 -0500

    Add lab solutions

---
+++ Block3/MadLibLab_Solved.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block3
+{
+	internal class MadLibLab
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Mad Lib Lab");
+
+			Console.WriteLine("Enter values to complete the mad lib.\n\n");
+			Console.Write("Enter a boy's name: ");
+			string boysName = Console.ReadLine();
+
+			Console.Write("Enter a noun: ");
+			string noun = Console.ReadLine();
+
+			Console.Write("Enter an occupation: ");
+			string occupation = Console.ReadLine();
+
+			Console.WriteLine("Enter an adjective: ");
+			string adjective = Console.ReadLine();
+
+			string madLib = string.Format("{0} is a valuable member of the Ministry of Magic." +
+				" Today {0} was in a rush to get to a meeting with the Minister of Magic" +
+				" and he forgot his {1}. This is problematic because when you work as a " +
+				"{2} you must have your {1} or you will have a really {3} day.", boysName,
+				noun, occupation, adjective);
+
+			Console.WriteLine(madLib);
+
+		}//end Main()
+	}//end Class
+}//end Namespace
---
+++ Block3/WaterLab_Solved.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block3
+{
+	internal class WaterLab
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Water Lab");
+
+			/*
+             * Write a program that will:
+             * 
+             *      - Assume that a gallon of water weighs 8.33 pounds
+             *      - Prompt the user to enter a number of gallons
+             *      - Display the total weight of the water in pounds
+             *      
+             * Test with something simple like 2 to start.
+             */
+
+			Console.Write("Enter a number of gallons: ");
+			decimal userGallons = decimal.Parse(Console.ReadLine());
+
+			decimal waterWeight = userGallons * 8.33m;
+
+			Console.WriteLine($"That weighs {waterWeight:n2} pounds.");
+		}//end Main()
+	}//end Class
+}//end Namespace
