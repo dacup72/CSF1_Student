5e7990eb843a544f7ceaa325fb2b9b13738a71db
Dylan Acup <dacup@ciscocems.com>
2023-06-07 14:47:26 -0500

    Add Loopinf For and While unsolved versions

---
+++ Block5/LoopingFor_Unsolved.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block5
+{
+	class LoopingForUnsolved
+	{
+		static void Main(string[] args)
+		{
+
+			//! Introduce Console.Title here
+
+			Console.WriteLine("Looping with FOR");
+			Console.Title = "-====== LOOPING WITH FOR =====-";
+
+			/*
+             * FOR LOOP SYNTAX
+             * 
+             * for (COUNTER; CONDITION; UPDATE)
+             * {
+             *      //code to run
+             * }
+             * 
+             * Some books refer to COUNTER as INITIALIZER
+             * //! Mnemonic device: ICU 
+             * 
+             * Counter - the starting point where you initailize a 
+             * variable that is used to check the condition. The 
+             * counter gets updated after each pass through the loop.
+             *
+             * Condition - A Boolean statement that tells the loop
+             * when to stop. It is checked before each pass.
+             * 
+             * Update - the change to the counter after each 
+             *          pass of the loop (increment, decrement, etc.)
+             * 
+             * Remember: a for loop is best used when you know exactly 
+             *           how many times you want to run the code
+             *           
+             * For loops are common and useful in many algorithms 
+             * and methods in the C# language
+             */
+
+			//! Note below that the COUNTER is declared and initialized within the for loop and that it is only accessible within the loop.
+			// Make a standard for loop
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//start with 3 grapes and eat until you have had 15.
+			//Eat 3 at a time.
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//count to 20, but skip 13
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//The continue keyword stops the current iteration of the loop 
+			//and moves on to the next iteration. Let's do the same example 
+			//above, but with continue:
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//From user input ask the user how many cookies they want and make them each 1 cookie at a time
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//Running Total Variable
+			//Creating a variable in an outer scope allows you 
+			//to use it and make changes to it from an inner scope
+
+			//Start by creating your running total variable.
+			//This can be a numeric type or a string that you
+			//just tack info onto
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//Another example:
+			//we can loop through an array with a for statement
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+		}//end Main()
+	}//end class
+}//end namespace
---
+++ Block5/LoopingWhile_Unsolved.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block5
+{
+	internal class LoopingWhileUnsolved
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Looping While!");
+			Console.Title = "-====== LOOPING WITH WHILE =====-";
+
+			/*While loop - best for when you want to run code
+             * an indeterminable number of times
+             * (0 to ???)
+             * 
+             * COUNTER
+             * 
+             * while (CONDITION)
+             * {
+             *      //code to run
+             *      UPDATE
+             * }
+             */
+
+			// Make a while loop that will print out 5 cookies one at a time
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//because you can set the update conditionally
+			//it is easy to forget it... or to accidently
+			//create an infinite loop by using a bad condition
+			// Make a while loop with an infinite loop
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//Virtually every time you have an infinite loop
+			//you wrote the condition wrong.
+
+			//Write a while loop that starts with 10 bags of groceries
+			//and counts down as they are unloaded until there are none
+			//remaining.
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//It is most common to use a while loop with a bool
+			//for a counter as this allows us to conditionally
+			//change the bool for when we want to exit the loop
+			// make a while loop using a bool as the conditional statement
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+			//PROGRAMMER REAL WORLD EXAMPLE
+			//for Loops are great for a specific number of times to loop
+			//They are ideal when you know the COUNT
+
+			//While loops are great for an indeterminable number of times
+			//They are ideal when you know the CONDITION
+			// Ask the user if that want to play a game and use their conditional response to run a while loop
+			// ==================== INSERT CODE BELOW ====================
+
+
+
+
+		}//end Main()
+	}//end Class
+}//end Namespace
