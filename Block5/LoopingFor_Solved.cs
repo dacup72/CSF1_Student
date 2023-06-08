969035e46d89d2b035d86d8ae34053def2db4251
Dylan Acup <dacup@ciscocems.com>
2023-06-07 22:18:14 -0500

    Add looping for and while solved versions

---
+++ Block5/LoopingFor_Solved.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block5
+{
+	class LoopingForSolved
+	{
+		static void Main(string[] args)
+		{
+
+			//! Introduce Console.Title here
+
+			Console.WriteLine("Looping with FOR");
+			Console.Title = "-====== LOOPING WITH FOR =====-";
+
+			/*
+             * FOR LOOP SYNTAX
+             * 
+             * for (COUNTER; CONDITION; UPDATE)
+             * {
+             *      //code to run
+             * }
+             * 
+             * Some books refer to COUNTER as INITIALIZER
+             * //! Mnemonic device: ICU 
+             * 
+             * Counter - the starting point where you initailize a 
+             * variable that is used to check the condition. The 
+             * counter gets updated after each pass through the loop.
+             *
+             * Condition - A Boolean statement that tells the loop
+             * when to stop. It is checked before each pass.
+             * 
+             * Update - the change to the counter after each 
+             *          pass of the loop (increment, decrement, etc.)
+             * 
+             * Remember: a for loop is best used when you know exactly 
+             *           how many times you want to run the code
+             *           
+             * For loops are common and useful in many algorithms 
+             * and methods in the C# language
+             */
+
+			//! Note below that the COUNTER is declared and initialized within the for loop and that it is only accessible within the loop.
+			for (int candyBar = 1; candyBar <= 7; candyBar++)//unary operator
+			{
+				Console.WriteLine("Yum! You have had " + candyBar);
+			}//end for
+
+			//start with 3 grapes and eat until you have had 15.
+			//Eat 3 at a time.
+			for (int grapes = 3; grapes <= 15; grapes += 3)
+			{
+				//could have also done the update as
+				//cookie = cookie + 3
+				Console.WriteLine("You ate three grapes and have "
+					+ "now had " + grapes + "!");
+			}//end for
+
+
+			//count to 20, but skip 13
+			for (int count = 1; count <= 20; count++)
+			{
+				if (count != 13)
+				{
+					Console.WriteLine(count);
+				}
+			}//end for
+
+			//The continue keyword stops the current iteration of the loop 
+			//and moves on to the next iteration. Let's do the same example 
+			//above, but with continue:
+			for (int count = 1; count <= 20; count++)
+			{
+				if (count == 13)
+				{
+					continue;
+				}
+				Console.WriteLine(count);
+			}
+
+
+			//From user input
+			Console.Write("\nHow many cookies do you want? ");
+			int userCookies = int.Parse(Console.ReadLine());
+
+			for (int cookie = 1; cookie <= userCookies; cookie++)
+			{
+				Console.WriteLine("You ate a cookie! " + cookie);
+			}//end for
+
+			//Running Total Variable
+			//Creating a variable in an outer scope allows you 
+			//to use it and make changes to it from an inner scope
+
+			//Start by creating your running total variable.
+			//This can be a numeric type or a string that you
+			//just tack info onto
+			string listOfNames = "";//typically called an empty string
+
+			Console.WriteLine("\n\nREGISTRATION\n");
+			Console.Write("How many people would you like to " +
+				"register for Comic Con? ");
+			int totalReg = Convert.ToInt32(Console.ReadLine());
+
+			for (int reg = 0; reg < totalReg; reg++)
+			{
+				Console.Write("Please enter a name: ");
+				listOfNames += Console.ReadLine() + "\n";
+			}//end for
+
+			Console.WriteLine($"\nYou have registered:\n{listOfNames}");
+			//Another example:
+
+
+			//we can loop through an array with a for statement
+			decimal[] prices = { 15.95m, 9.99m, 20, 10, 9.75m };
+
+			for (int i = 0; i < prices.Length; i++)
+			{
+				Console.WriteLine("At the index of {0} is {1:c}",
+					i, prices[i]);
+			}//end for
+
+			Console.WriteLine("The total is: {0:c}", prices.Sum());
+
+		}//end Main()
+	}//end class
+}//end namespace
---
+++ Block5/LoopingWhile_Solved.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block5
+{
+	internal class LoopingWhileSolved
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Looping While!");
+			Console.Title = "-====== LOOPING WITH WHILE =====-";
+
+			/*While loop - best for when you want to run code
+             * an indeterminable number of times
+             * (0 to ???)
+             * 
+             * COUNTER
+             * 
+             * while (CONDITION)
+             * {
+             *      //code to run
+             *      UPDATE
+             * }
+             */
+
+			int cookie = 1;//COUNTER
+
+			while (cookie <= 5)//CONDITION.. NO SEMICOLON
+			{
+				//code to run
+				Console.WriteLine("Yum! You have eaten " + cookie);
+
+				cookie++;//UPDATE
+			}//end while
+
+			//because you can set the update conditionally
+			//it is easy to forget it... or to accidently
+			//create an infinite loop by using a bad condition
+
+			//long number = 1;//COUNTER
+
+			//while (number > 0)//CONDITION
+			//{
+			//    Console.WriteLine(number);
+			//    number++;//UPDATE
+			//}//end while
+
+			//Virtually every time you have an infinite loop
+			//you wrote the condition wrong.
+
+			//Write a while loop that starts with 10 bags of groceries
+			//and counts down as they are unloaded until there are none
+			//remaining.
+			int groceryBags = 10;
+
+			while (groceryBags > 0)
+			{
+				Console.WriteLine("You have {0} bags of groceries left."
+					+ " You are now unloading one!",
+					groceryBags);
+				groceryBags--;
+			}//end while
+
+			//It is most common to use a while loop with a bool
+			//for a counter as this allows us to conditionally
+			//change the bool for when we want to exit the loop
+
+			bool moreBags = true;//COUNTER
+
+			while (moreBags)//CONDITION just needs to be true
+			{
+				Console.WriteLine("\nYou have unloaded some groceries.");
+
+				//ask the user if they have more bags! 
+				//If not, we are done!
+				Console.Write("Do you have more bags? Y/N: ");
+				//ConsoleKey...
+				ConsoleKey userAnswer = Console.ReadKey(true).Key;
+
+				if (userAnswer == ConsoleKey.N)
+				{
+					moreBags = false;//UPDATE
+				}//end if
+
+			}//end while
+
+			//PROGRAMMER REAL WORLD EXAMPLE
+			//for Loops are great for a specific number of times to loop
+			//They are ideal when you know the COUNT
+
+			//While loops are great for an indeterminable number of times
+			//They are ideal when you know the CONDITION
+
+			Console.Write("Would you like to play a game? Y/N: ");
+			ConsoleKey runProgram = Console.ReadKey(true).Key; //Counter
+
+			while (runProgram == ConsoleKey.Y)
+			{
+				Console.WriteLine("\nYou win!");
+
+				Console.Write("Play again? Y/N: ");
+				runProgram = Console.ReadKey(true).Key;
+			}//end while
+
+
+
+
+
+			//// Instructor Dylan Example
+			// Console.WriteLine("Do you want to play a game?  EX: \"y\" or \"yes\"");
+			// string userChoice = Console.ReadLine().ToLower();
+
+			// //bool playGame = userChoice == "y" || userChoice == "yes" ? true : false;
+			// bool playGame = userChoice == "y" || userChoice == "yes";
+
+			// while(playGame)
+			// {
+			//     Console.WriteLine("Which game do you want to play?\n1) Frogger\n2) Chess\n3) War");
+			// 	string gameChoice = Console.ReadLine().ToUpper();
+
+			// 	if(gameChoice == "FROGGER")
+			// 	{
+			//         Console.WriteLine("You played frogger!!!");
+			//     }
+			// 	else if (gameChoice == "CHESS")
+			// 	{
+			//         Console.WriteLine("You played chess!!!");
+			//     }
+			// 	else if(gameChoice == "WAR")
+			// 	{
+			//         Console.WriteLine("You played War!!!");
+			//     }
+			// 	else
+			// 	{
+			//         Console.WriteLine("That is not a valid game");
+			//     }
+
+			//     Console.WriteLine("Do you want to play again?  EX: \"y\" or \"yes\"");
+			// 	string userChoice2 = Console.ReadLine().ToLower();
+			// 	playGame = userChoice2 == "y" || userChoice2 == "yes";
+			// }
+
+			// Console.WriteLine("Game Over!");
+
+		}//end Main()
+	}//end Class
+}//end Namespace
