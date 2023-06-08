5d955010c9fc345d2466e6918fc29a587ffbc729
Dylan Acup <dacup@ciscocems.com>
2023-06-08 16:26:39 -0500

    Update looping do while and foreach unsolved versions

--- Block5/LoopingForeach_Unsolved.cs
+++ Block5/LoopingForeach_Unsolved.cs
@@ -33,61 +33,45 @@ namespace Block5
 
 			string[] colors = { "red", "blue", "green", "purple",
 							  "black", "pink"};
+			// Loop through the array to print out all items
+			// ==================== INSERT CODE BELOW ====================
 
-			foreach (string color in colors)
-			{
-				Console.WriteLine(color);
-			}//end foreach
+
 
 
 			int[] gradeScores = { 100, 80, 42, 75, 2 };
+			// Loop through the array to print out all items
+			// ==================== INSERT CODE BELOW ====================
 
-			foreach (int bob in gradeScores)
-			{
-				Console.WriteLine(bob);
-			}//end foreach
+
+
+			
 
 			//Print out each of the prices in the below array to the screen with currency formatting:
-
 			decimal[] cartPrices = { 12.99m, 2, 9.99m, 10, 20 };
+			// ==================== INSERT CODE BELOW ====================
 
-			//print out each price with a foreach structure
-			foreach (decimal price in cartPrices)
-			{
-				Console.WriteLine("{0:c}", price);
-			}//end foreach
+
 
 
 			//Let's total up all of the prices in the cart.
 			//Create a running total variable so we can
 			//print out the total AFTER the foreach.
+			// ==================== INSERT CODE BELOW ====================
 
-			decimal totalSale = 0;//always give a running total variable
-								  //some default (starting) value
 
-			Console.WriteLine("\nThank you for purchasing from WeRProgrammers\n");
 
-			foreach (decimal price in cartPrices)
-			{
-				//totalSale = totalSale + price;
-				totalSale += price;
-				//either of the above works.. but only choose one...
-				Console.WriteLine("After adding {0:c}, the total is now: {1:c}",
-					price, totalSale);
 
-			}//end foreach
 
-			Console.WriteLine("\nYour total purchase is: {0:c}", totalSale);
 
 			//Remember, a string is a collection of characters
 			string letters = "abcdefghijklmnopqrstuvwxyz";
+			// Print out each character
+			// ==================== INSERT CODE BELOW ====================
 
-			foreach (char letter in letters)
-			{
-				Console.WriteLine(letter);
-			}
 
-			Console.WriteLine(letters.Length + " total letters");
+
+
 		}//end Main()
 	}//end Class
 }//end Namespace
